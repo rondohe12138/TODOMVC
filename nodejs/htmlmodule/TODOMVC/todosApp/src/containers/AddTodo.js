042fcfe2937232314a047464f0b68f823348eb61
rondohe12138 <63772406+rondohe12138@users.noreply.github.com>
2021-01-03 21:18:08 +0800

    Update AddTodo.js

--- nodejs/htmlmodule/TODOMVC/todosApp/src/containers/AddTodo.js
+++ nodejs/htmlmodule/TODOMVC/todosApp/src/containers/AddTodo.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { addTodo } from '../actions';
-// TODO: AddTodo
+
 const AddTodo = ({ dispatch }) => {
         let input;
-
         function store(namespace, data) {
             if (data) {
                 return localStorage.setItem(namespace, JSON.stringify(data));
@@ -13,11 +12,10 @@ const AddTodo = ({ dispatch }) => {
             var store = localStorage.getItem(namespace);
             return (store && JSON.parse(store)) || [];
         };
-        return ( <
-            div >
-            <
-            h3 > TODOMVC < /h3> <
-            form onSubmit = {
+        return ( 
+                <div>
+            <h3>TODOMVC</h3>
+                <form onSubmit = {
                 e => {
                     e.preventDefault();
                     if (!input.value.trim()) {
@@ -27,14 +25,11 @@ const AddTodo = ({ dispatch }) => {
                     store(namespace, input.value)
                     input.value = ''
                 }
-            } >
-            <
-            input ref = { node => input = node }
-            />{ ' ' } <
-            button type = "submit" > 新增任务项 < /button> <
-            /form> <
-            /div>
+            }>
+            <input aref = { node => input = node }/>{ ' ' }
+            <button type = "submit">任务项</button>
+                </form> 
+                </div>
         );
     }
-    // TODO: export
 export default connect()(AddTodo);
