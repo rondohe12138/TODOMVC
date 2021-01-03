70da4f1c02bb63ed7c47822c2858977390aa1a92
rondohe12138 <63772406+rondohe12138@users.noreply.github.com>
2021-01-03 21:19:34 +0800

    Update VisibleTodoList.js

--- nodejs/htmlmodule/TODOMVC/todosApp/src/containers/VisibleTodoList.js
+++ nodejs/htmlmodule/TODOMVC/todosApp/src/containers/VisibleTodoList.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 import { toggleTodo } from '../actions';
 import TodoList from '../components/TodoList';
 import { VisibilityFilters } from '../actions';
-// TODO: getVisibleTodos
+
 const getVisibleTodos = (todos, filter) => {
   switch (filter) {
     case VisibilityFilters.SHOW_ALL:
@@ -15,15 +15,15 @@ const getVisibleTodos = (todos, filter) => {
       throw new Error('Unknown filter: ' + filter);
   }
 }
-// TODO: mapStateToProps
+
 const mapStateToProps = state => ({
   todos: getVisibleTodos(state.todos, state.visibilityFilter)
 });
-// TODO: mapDispatchToProps
+
 const mapDispatchToProps = dispatch => ({
   toggleTodo: id => dispatch(toggleTodo(id))
 });
-// TODO: export
+
 export default connect(
   mapStateToProps,
   mapDispatchToProps
