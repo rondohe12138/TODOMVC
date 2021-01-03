9bfff6bbbb5dc5d05b1326c51d1801b36c78351c
rondohe12138 <63772406+rondohe12138@users.noreply.github.com>
2021-01-03 21:20:06 +0800

    Update App.js

--- nodejs/htmlmodule/TODOMVC/todosApp/src/components/App.js
+++ nodejs/htmlmodule/TODOMVC/todosApp/src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Footer from './Footer';
 import AddTodo from '../containers/AddTodo';
 import VisibleTodoList from '../containers/VisibleTodoList';
-// TODO: App
+
 const App = () => (
   <div>
     <AddTodo />
@@ -10,5 +10,5 @@ const App = () => (
     <Footer />
   </div>
 );
-// TODO: export App
+
 export default App;
