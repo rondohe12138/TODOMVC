452d43f4e540d2a5ee6fa0d23d46c41ffd5067be
rondohe12138 <63772406+rondohe12138@users.noreply.github.com>
2021-01-03 21:13:58 +0800

    Update index.js

--- nodejs/htmlmodule/TODOMVC/todosApp/src/reducers/index.js
+++ nodejs/htmlmodule/TODOMVC/todosApp/src/reducers/index.js
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux';
 import todos from './todos';
 import visibilityFilter from './visibilityFilter';
-// TODO: export
 export default combineReducers({
   todos,
   visibilityFilter
